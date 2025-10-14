a54e15ac59943db1790f6873873b019672e878d0
restenPiz <mauropeniel7@gmail.com>
2025-10-14 23:08:09 +0200

    Defining the authentication routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\LoginController;
 
+//*First route
 Route::get('/', [LoginController::class, 'index']);
+
+//*Authentication routes
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout']);
