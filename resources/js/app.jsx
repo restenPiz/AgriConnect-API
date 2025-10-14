723268aaf0e42833e01c89fd220d6b446087e0b3
restenPiz <mauropeniel7@gmail.com>
2025-10-14 22:43:44 +0200

    Ever bugs are solved right now. We can procced with another steps

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,7 +4,8 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    // resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) => resolvePageComponent(`./${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     },
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,30 @@ export default {
   darkMode: ["class"],
   content: [
     "./resources/**/*.blade.php",
-    "./resources/**/*.jsx",
     "./resources/**/*.js",
+    "./resources/**/*.jsx",
+    "./resources/**/*.vue",
+    "./resources/**/*.ts",
+    "./resources/**/*.tsx",
   ],
   theme: {
     extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-      colors: {},
     },
   },
   plugins: [require("tailwindcss-animate")],
