40e2c2fb93ec10a8b44a2ea30cb2dd9d74ad5182
restenPiz <mauropeniel7@gmail.com>
2025-10-22 10:59:04 +0200

    Working on UserController

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $user = new User();
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = $request->input('password');
+        $user->phone_number = $request->input('phone_number');
+        $user->location = $request->input('location');
+
+        $user->save();
+
+        return true;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
