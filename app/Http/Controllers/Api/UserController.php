91e494b04a31aa926196700b40bac9991311adbf
restenPiz <mauropeniel7@gmail.com>
2025-10-22 11:05:45 +0200

    Creating a api route for createAccount method

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -23,25 +23,16 @@ class UserController extends Controller
         return true;
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//*Start with the api Routes
+Route::get('/createAccount', [UserController::class, 'store'])->name('createAccount');
