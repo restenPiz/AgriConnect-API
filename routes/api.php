2969e5b8c1de79d5fb302e33f13cc0a9cbb41c00
restenPiz <mauropeniel7@gmail.com>
2025-11-17 12:52:52 +0200

    Doing some updates in the routes file

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\Api\ProductController;
 
 //*Start with the api Routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/userData/{id}', [AuthController::class, 'user']);
 //* Protected routes - Requires authentication
 Route::middleware('auth:sanctum')->group(function () {
+
+    //*Authentication Routes
     Route::post('/logout', [AuthController::class, 'logout']);
-    // Route::get('/user', [AuthController::class, 'user']);
+    Route::get('/userData/{id}', [AuthController::class, 'user']);
     Route::post('/user/profile', [AuthController::class, 'updateProfile']);
+
+    //*Product Routes
+    Route::post('/storeProduct', [ProductController::class, 'store']);
 });
