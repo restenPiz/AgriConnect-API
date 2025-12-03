32bfa14b7548671226f8b7a37d21dddd2c8ceaa9
restenPiz <mauropeniel7@gmail.com>
2025-12-03 07:07:22 +0200

    Add cooperatives routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CooperativeController;
 
 //*Start with the api Routes
 Route::post('/register', [AuthController::class, 'register']);
@@ -25,3 +26,23 @@ Route::get('/product/{id}', [ProductController::class, 'getByFarmer']);
 Route::get('/productEdit/{id}', [ProductController::class, 'edit']);
 Route::post('/productUpdate/{id}', [ProductController::class, 'update']);
 Route::delete('/productDelete/{id}', [ProductController::class, 'destroy']);
+
+//*Cooperative Routes
+Route::get('/cooperatives', [CooperativeController::class, 'index']);
+Route::post('/cooperatives', [CooperativeController::class, 'store']);
+Route::get('/cooperatives/{id}', [CooperativeController::class, 'show']);
+Route::post('/cooperatives/{id}', [CooperativeController::class, 'update']);
+Route::delete('/cooperatives/{id}', [CooperativeController::class, 'destroy']);
+
+// Cooperative Members
+Route::get('/cooperatives/{id}/members', [CooperativeController::class, 'getMembers']);
+Route::post('/cooperatives/{id}/members', [CooperativeController::class, 'addMember']);
+Route::delete('/cooperatives/{id}/members/{memberId}', [CooperativeController::class, 'removeMember']);
+
+// Available Farmers (for adding to cooperatives)
+Route::get('/farmers/available', function () {
+    $farmers = \App\Models\User::where('user_type', 'farmer')
+        ->select('id', 'name', 'email')
+        ->get();
+    return response()->json(['success' => true, 'data' => $farmers]);
+});
